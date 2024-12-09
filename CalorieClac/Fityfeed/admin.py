8b71bb7eef41fcf0187e43d028dda9327c2bf79c
fahlev-dev <rfahlevi501@gmail.com>
2024-12-09 21:54:14 +0700

    membuat aplikasi kalkulator kalori

---
+++ CalorieClac/Fityfeed/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+class foodAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Fooditem
+    list_display = ['name']
+    list_filter = ['name']
+        
+admin.site.register(Customer)
+admin.site.register(UserFoodItem)
+admin.site.register(Category)
+admin.site.register(Fooditem, foodAdmin)
+    
